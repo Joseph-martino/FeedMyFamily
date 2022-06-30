4dfe9c1318105e799e1a0fa521426364f2b6c301
Shayoon <quentin.degorgue@gmail.com>
2022-06-30 13:22:58 +0200

    🚧initialisation du projet

---
+++ src/main/java/com/project/feedmyfamily/FeedmyfamilyApplication.java
@@ -0,0 +1,13 @@
+package com.project.feedmyfamily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedmyfamilyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedmyfamilyApplication.class, args);
+	}
+
+}
